043c82a1d6cea3a72f121afa1ddf10c8947401d5
Selena <selena.k.rodriguez@gmail.com>
2023-07-11 22:00:40 -0500

    Added functionality to add skills and see the previous skills added

--- cv-project/src/components/CVForm/Skills.js
+++ cv-project/src/components/CVForm/Skills.js
@@ -1,12 +1,90 @@
 import React, { Component } from "react";
 import InputField from "../Utilities/InputField";
+import Tasks from "../Utilities/Tasks";
+import Button from "../Utilities/Button";
+// import uniqid from "uniqid";
 
 class Skills extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      skill: "",
+      skills: [],
+      isEditing: false,
+      isSkillAdded: false,
+    };
+  }
+
+  handleAddSkill = () => {
+    this.setState({
+      isEditing: true,
+      isSkillAdded: true,
+    });
+  };
+
+  handleEditClick = () => {
+    this.setState({
+      isEditing: true,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { skills, skill } = this.state;
+
+    if (skill.trim() !== "") {
+      this.setState({
+        skills: skills.concat(skill),
+        skill: "",
+        isEditing: false,
+      });
+    }
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      skill: e.target.value,
+    });
+  };
+
   render() {
+    const { skills, skill, isEditing, isSkillAdded } = this.state;
     return (
-      <>
-        <InputField label="info" type="text" />
-      </>
+      <div>
+        <h2>Skills</h2>
+        {!isSkillAdded ? (
+          <Button text="+ Add Skill" onClick={this.handleAddSkill} />
+        ) : isEditing ? (
+          <div id="skillInputs" className="inputValues">
+            <Tasks skills={skills} />
+            <form
+              onSubmit={this.handleSubmit}
+              id="skillsEdit"
+              className="editForm"
+              action=""
+              method="GET"
+            >
+              <InputField
+                label=""
+                id="skills"
+                type="text"
+                value={skill}
+                onChange={this.handleInputChange}
+                placeholder=""
+              />
+              <div className="submitButton">
+                <Button text="Save" type="submit" />
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div onClick={this.handleEditClick}>
+            {skills.length > 0 && <Tasks skills={skills} />}
+            <Button text="+ Add Skill" onClick={this.handleAddSkill} />
+          </div>
+        )}
+      </div>
     );
   }
 }
